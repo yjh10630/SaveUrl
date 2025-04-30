c4d14b766ece79e36db87d74e989980aad4712ee
YunJiHun <yjhzzing4699@naver.com>
2025-04-30 16:41:23 +0900

    dependencies - crashlytics 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.dagger.hilt.android") version "2.48" apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.22" apply false
+
     id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.3" apply false
 }
