d75a8a54826922ef5ec5759000d33119eaea81fd
YunJiHun <yjhzzing4699@naver.com>
2025-06-11 14:04:20 +0900

    테스트 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.jinscompany.saveurl"
         minSdk = 24
         targetSdk = 34
-        versionCode = 30
-        versionName = "1.0.5"
+        versionCode = 31
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["AdMobId"] = adsId
